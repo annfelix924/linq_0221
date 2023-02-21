af197e5042dcb3ec72056d19634b338c08c7c200
annfelix <a1314131413141314@gmail.com>
2023-02-21 13:18:51 +0800

    加入專案檔案。

---
+++ linq/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace linq
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("輸入一個用逗號區隔的整數數列：");
+            var input = Console.ReadLine();
+            var numbers = (input.Split(',').Reverse());
+            Console.WriteLine(string.Join(",", numbers));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ linq_2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace linq_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("輸入一個用逗號區隔的整數數列：");
+            var input = Console.ReadLine();
+            var numbers = input.Split(',').Select(int.Parse).OrderBy(n=>n);
+            var oddNumbers = numbers.Where(n => n % 2 == 1);
+            var evenNumbers = numbers.Where(n => n % 2 == 0);
+            Console.WriteLine("奇數: " + string.Join(",", oddNumbers));
+            Console.WriteLine("偶數: " + string.Join(",", evenNumbers));
+            Console.ReadLine();
+         }
+     }
+
+}
+
